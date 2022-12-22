6c93acde1e4344bf3999345be2224405bf6d7fa1
steven-mi <stevenmi@live.de>
2022-12-22 01:35:45 +0100

    init

---
+++ src/test/java/com/example/demoproducer/DemoProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
